d52235c73d2bccffb71ddf6cc117761739240e77
Carlos Rodrigues <carlos.all.mail@gmail.com>
2020-05-14 10:50:22 -0600

    Installed Dox

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -62,3 +62,18 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+# Dox generates API documentation from Rspec controller/request specs
+# see: https://github.com/infinum/dox
+require 'dox'
+RSpec.configure do |config|
+  config.after(:each, :dox) do |example|
+    example.metadata[:request] = request
+    example.metadata[:response] = response
+  end
+end
+Dox.configure do |config|
+  config.header_file_path = Rails.root.join('spec/docs/v1/descriptions/header.md')
+  config.desc_file_path = Rails.root.join('spec/docs/v1/descriptions')
+  config.headers_whitelist = ['Accept', 'X-Auth-Token']
+end
